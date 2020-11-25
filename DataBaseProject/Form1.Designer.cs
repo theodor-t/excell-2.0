58e189c557b696a509cf3b6516b2e9786c6f9bcb
Theodor <54949643+theodorerlax@users.noreply.github.com>
2020-11-25 21:00:02 +0200

    Add files via upload

--- DataBaseProject/Form1.Designer.cs
+++ DataBaseProject/Form1.Designer.cs
@@ -160,6 +160,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(3, 3);
             this.dataGridView1.Name = "dataGridView1";
